5ecf123f235e8c560a1b77c51774d11961f4fbeb
Hendry Sadrak <hendry.sadrak@outlook.com>
2022-02-02 15:50:03 +0200

    upgrade to yarn

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
-import test from 'ava';
-import keyvTestSuite from '@keyv/test-suite';
-import Keyv from 'keyv';
-import KeyvMongoDB from '..';
+import test from "ava";
+import keyvTestSuite from "@keyv/test-suite";
+import Keyv from "keyv";
+import KeyvMongoDB from "..";
 
-const store = () => new KeyvMongoDB({ url: 'mongodb://127.0.0.1:27017/keyv-mongodb-test' });
+const store = () =>
+	new KeyvMongoDB({ url: "mongodb://127.0.0.1:27017/keyv-mongodb-test" });
 
-keyvTestSuite(test, Keyv, store);
+keyvTestSuite.default(test, Keyv, store);
 
 // test('Collection option merges into default options', t => {
 // 	const store = new KeyvMongoDB({ collection: 'foo' });
